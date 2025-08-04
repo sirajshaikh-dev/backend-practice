a53c7bc69d1bd5118d0fd9bfe8b7fed189e8c072
sirajshaikh-dev <ss1425306@gmail.com>
2025-08-04 22:04:01 +0530

    feat: implement user registration logic with validation and token generation

--- 02_cohort/src/controllers/auth.controller.js
+++ 02_cohort/src/controllers/auth.controller.js
@@ -1,6 +1,45 @@
+import { User } from "../models/user.model.js";
+import crypto from 'crypto'
+
 const register = async (req, res) => {
   res.send("user registered");
+
+//   1. get data
+//   2. validate data -> All fields are required
+//   3. check if user already exists
+//   4. create a user in database
+//   5. create a verification token
+//   6. save token in database
+//   7. send token as email to user
+//   8. send success status to user
+
+  const { name, email, password } = req.body;
+
+  if(!name || !email ||!password ){
+    return res.status(400).json({
+        message: "All fields required"
+    })
+  }
+
+  //check if user exists
+  const existingUser = await User.findOne({email})
+  if(existingUser) return res.status(400).json({message: "User already registered"})
+  
+    // create new user in db
+  const newUser = await User.create({name,email,password})
+  if(!newUser) return res.status(400).json({message: "User not registered"})
+
+    // Create a verificatinToken 
+    const token = crypto.randomBytes(32).toString('hex')
+    newUser.verificationToken= token
+
+    //save token in  database
+    await newUser.save()
 };
+
+
+
+
 const login = async (req, res) => {
   res.send("user login success");
 };
